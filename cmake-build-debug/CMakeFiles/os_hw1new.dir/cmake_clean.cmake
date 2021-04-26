f2a8c399872957a3dfca4f8c399efccb0ce904a7
Ortal1993 <cohen1993@gmail.com>
2021-04-26 06:46:04 +0000

    bg done

---
+++ cmake-build-debug/CMakeFiles/os_hw1new.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/os_hw1new.dir/Commands.cpp.o"
+  "CMakeFiles/os_hw1new.dir/signals.cpp.o"
+  "CMakeFiles/os_hw1new.dir/smash.cpp.o"
+  "os_hw1new"
+  "os_hw1new.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/os_hw1new.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
